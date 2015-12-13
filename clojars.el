836da7455f3662eebef14fad3e114b98fff3ad60
Александар Симић <asimic@gmail.com>
2015-12-13 21:10:30 +0000

    Made it a package
    Seeing how most of the key code was already there, I added the
    following:
    
    - renamed 'clojars-search' to 'clojars' as that's what the package name
      is and that is the main functionality of the package, so it seems to
      me that it would be easier to type just 'clojars'
    
    - namespaced all the symbols with the library namespace 'clojars-' in
      order to avoid any potential clashes
    
    - added the constant for the clojars search path
    
    - added the magic cookie for to autoload the clojar function
    
    - made it a package by adding the header, usage example and the footer

--- clojars.el
+++ clojars.el
@@ -1,33 +1,60 @@
+;;; clojars.el --- clojars.org search interface
+
+;; Filename: clojars.el
+;; Description: clojars.org search interface
+;; Author: Joshua Miller <josh@joshmiller.io>
+;; License: GPLv3
+;; Created: 2014-12-15 17:21:05
+;; Version: x.y.z
+;; Package-Version: ADD THIS e.g. 20151213.xy
+;; URL: https://github.com/joshuamiller/clojars.el
+;; Keywords: docs, help, tools
+
+;; This file in not part of GNU Emacs.
+
+;; Commentary:
+
+;; M-x clojars - to search clojars.org for a Clojure library
+
+;;; Code:
+
 (require 'request)
 (require 'json)
 (require 'ido)
 
-(defun jar-name (result)
+(defconst clojars-search-endpoint "https://clojars.org/search"
+  "Clojars search endpoint")
+
+(defun clojars-jar-name (result)
   (let ((group-name (cdr (assoc 'group_name result)))
         (jar-name (cdr (assoc 'jar_name result))))
     (if (string= group-name jar-name)
         jar-name
       (format "%s/%s" group-name jar-name))))
 
-(defun format-dependency (result)
+(defun clojars-format-dependency (result)
   (let ((version (cdr (assoc 'version result)))
-        (name (jar-name result)))
+        (name (clojars-jar-name result)))
     (format "[%s %S]" name version)))
 
-(defun jar-result (result)
-  (cons (format-dependency result) (jar-name result)))
+(defun clojars-jar-result (result)
+  (cons (clojars-format-dependency result) (clojars-jar-name result)))
 
-(defun clojars-search (query)
-  "Finds a Clojars dependency, and copies selected result to kill ring"
+;;;###autoload
+(defun clojars (query)
+  "Finds a Clojure library from clojars.org, and copies selected
+   result to kill ring"
   (interactive "sSearch Clojars: ")
   (message "Loading...")
   (request
-   "http://clojars.org/search"
+   clojars-search-endpoint
    :params `(("q" . ,query) ("format" . "json"))
    :parser 'json-read
    :success (function*
              (lambda (&key data &allow-other-keys)
                (let ((results (cdr (assoc 'results data))))
-                 (kill-new (ido-completing-read "Results: " (mapcar 'jar-result results))))))))
+                 (kill-new (ido-completing-read "Results: " (mapcar 'clojars-jar-result results))))))))
 
-(provide 'clojars-search)
+(provide 'clojars)
+
+;;; clojars.el ends here
